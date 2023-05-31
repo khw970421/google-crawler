1c1042c5e73900bab90cf7daaa0ad75f73ee34d9
khw970421 <khw970421@kakao.com>
2023-05-31 19:08:01 +0900

    AuthCode 얻는 url 처리

---
+++ getUrl.js
@@ -0,0 +1,26 @@
+const { google } = require('googleapis')
+require('dotenv').config()
+
+// const credentials = require('./credentials.json')
+
+// 1.
+
+const client = new google.auth.OAuth2(
+  process.env.CLIENT_SECRET,
+  process.env.CLIENT_ID,
+  process.env.REDIRECT_URL
+)
+
+const SCOPES = ['https://www.googleapis.com/auth/drive'] // 사용할 API에 따라 권한 범위를 설정합니다.
+let token = ''
+
+const getAuthUrl = () => {
+  const authUrl = client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  })
+
+  console.log('Authorize this app by visiting this URL:', authUrl)
+}
+
+getAuthUrl()
