ce65e15326898e49c52701c71abc1d64c27b0ba8
khw970421 <khw970421@kakao.com>
2023-06-01 18:30:56 +0900

    파일 업로드 기능구현 - google api limit 제한 존재

--- uploadFile.js
+++ uploadFile.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 // url을 통해 얻은거  authcode 질문
 const url =
-  'https://example.com/?code=4%2F0AbUR2VP6_hLLaTrWw8RujhuCqzrOfC-_MSVaRHS_UEV_Q-rn_UdK50M88T9p1JoWN7gXAA&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive'
+  'https://example.com/?code=4%2F0AbUR2VMd2kQXjP_I433tseUpY7aNGLWeY4PA5eUs5JYlH8jzk3UidVqzR2xrtKK444dL3A&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive'
 
 const client = new google.auth.OAuth2(
   process.env.CLIENT_SECRET,
@@ -27,66 +27,31 @@ const getToken = () => {
 }
 
 async function uploadImage(client) {
-  try {
-    const drive = google.drive({ version: 'v3', auth: client })
-    const filePath = './다운로드.jpg'
-    const fileMetadata = {
-      name: '파일 이름',
-      mimeType: 'image/jpeg',
-    }
-    const media = {
-      mimeType: 'image/jpeg',
-      body: fs.createReadStream(filePath),
-    }
-    const response = await drive.files.create({
-      requestBody: fileMetadata,
-      media: media,
+  var files = fs.readdirSync('img')
+  files.forEach((file) => {
+    const f = fs.readdirSync(`img/${file}`)
+    f.forEach(async (jpg) => {
+      try {
+        const drive = google.drive({ version: 'v3', auth: client })
+        const filePath = `img/${file}/${jpg}`
+        const fileMetadata = {
+          name: '파일 이름',
+          mimeType: 'image/jpeg',
+        }
+        const media = {
+          mimeType: 'image/jpeg',
+          body: fs.createReadStream(filePath),
+        }
+        const response = await drive.files.create({
+          requestBody: fileMetadata,
+          media: media,
+        })
+        console.log('File uploaded:', response.data)
+      } catch (error) {
+        console.error('Error uploading file:', error)
+      }
     })
-    console.log('File uploaded:', response.data)
-  } catch (error) {
-    console.error('Error uploading file:', error)
-  }
+  })
 }
 
 getToken()
-
-/*
-const { google } = require('googleapis')
-const fs = require('fs')
-const { getQueryImgs } = require('./getCrawl')
-const { frontKeyword, keywordQueries } = require('./data/keywordQueries')
-const { addFrontString } = require('./utils/func')
-require('dotenv').config()
-
-// url을 통해 얻은거  authcode 질문
-const url =
-  'https://example.com/?code=4%2F0AbUR2VNx5bH2ITTIdzJ5J0RZmnyvrMwDryiu06NBs9taJi_Ka9lz194eOldfXqnfG5lFyg&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive'
-
-const client = new google.auth.OAuth2(
-  process.env.CLIENT_SECRET,
-  process.env.CLIENT_ID,
-  process.env.REDIRECT_URL
-)
-
-const crawlQuery = addFrontString(frontKeyword, keywordQueries)
-
-const getToken = () => {
-  const authCode = decodeURIComponent(
-    url.split('&scope')[0].split('?code=')[1]
-  ).slice()
-  const func = async () => {
-    const { tokens } = await client.getToken(authCode)
-    console.log(tokens)
-    client.setCredentials(tokens)
-
-    crawlQuery.forEach(async (query) => {
-      getQueryImgs(query, client)
-    })
-  }
-
-  func()
-}
-
-getToken()
-
-*/
