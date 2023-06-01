9e480a9db069ef0dcdb5d8d87cc9fef474bb45dc
khw970421 <khw970421@kakao.com>
2023-06-02 01:39:55 +0900

    data:image, https 모두 download 처리

--- getCrawl.js
+++ getCrawl.js
@@ -11,22 +11,21 @@ const { google } = require('googleapis')
 const { frontKeyword, keywordQueries } = require('./data/keywordQueries')
 const { download } = require('./utils/download')
 const { makeDirectory, addFrontString } = require('./utils/func')
-const { uploadImage } = require('./utils/upload')
 
-async function getQueryImgs(query, client) {
+async function getQueryImgs(query) {
   const images = []
   const driver = await new Builder().forBrowser(Browser.CHROME).build()
 
   try {
     await driver.get(`https://www.google.com/search?q=${query}&tbm=isch`)
 
-    const scrollCount = 1 // 반복할 스크롤 횟수
-    for (let i = 0; i < scrollCount; i++) {
-      await driver.executeScript(
-        'window.scrollTo(0, document.body.scrollHeight)'
-      )
-      await driver.sleep(500)
-    }
+    // const scrollCount = 1 // 반복할 스크롤 횟수
+    // for (let i = 0; i < scrollCount; i++) {
+    //   await driver.executeScript(
+    //     'window.scrollTo(0, document.body.scrollHeight)'
+    //   )
+    //   await driver.sleep(500)
+    // }
 
     await driver.wait(until.urlContains('http'), 100)
 
@@ -34,9 +33,7 @@ async function getQueryImgs(query, client) {
 
     for (var i = 0; i < resultElements.length; i++) {
       const image = await resultElements[i].getAttribute('src')
-      if (image && image.startsWith('https')) {
-        uploadImage(client, image)
-
+      if (image) {
         images.push(image)
       }
     }
@@ -51,15 +48,9 @@ async function getQueryImgs(query, client) {
 
 makeDirectory('img')
 
-const client = new google.auth.OAuth2(
-  process.env.CLIENT_SECRET,
-  process.env.CLIENT_ID,
-  process.env.REDIRECT_URL
-)
-
 const crawlQuery = addFrontString(frontKeyword, keywordQueries)
 crawlQuery.forEach(async (query) => {
-  getQueryImgs(query, client)
+  getQueryImgs(query)
 })
 
 exports.getQueryImgs = getQueryImgs
--- utils/download.js
+++ utils/download.js
@@ -8,14 +8,38 @@ var download = function (urls, fileNames, directoryName, callback) {
   urls.forEach((url, idx) => {
     console.log(url)
 
-    request.head(url, function (err, res, body) {
-      console.log('content-type:', res.headers['content-type'])
-      console.log('content-length:', res.headers['content-length'])
+    if (url.startsWith('https')) {
+      request.head(url, function (err, res, body) {
+        console.log('content-type:', res.headers['content-type'])
+        console.log('content-length:', res.headers['content-length'])
 
-      request(url)
-        .pipe(fs.createWriteStream(`./img/${directoryName}/${idx}.jpg`))
-        .on('close', callback)
-    })
+        request(url)
+          .pipe(fs.createWriteStream(`./img/${directoryName}/${idx}.jpg`))
+          .on('close', callback)
+      })
+    } else {
+      console.log('else')
+      const imageParts = url.split(';base64,')
+      const mimeType = imageParts[0].split(':')[1]
+      const imageData = imageParts[1]
+
+      // Base64 디코딩
+      const decodedImage = Buffer.from(imageData, 'base64')
+
+      // 이미지 파일로 저장
+      fs.writeFile(
+        `./img/${directoryName}/${idx}.jpg`,
+        decodedImage,
+        { encoding: 'base64' },
+        (err) => {
+          if (err) {
+            console.error('이미지 저장 실패:', err)
+          } else {
+            console.log('이미지 저장 성공!')
+          }
+        }
+      )
+    }
   })
 }
 
