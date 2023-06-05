6991e39a294314bf03bc2b87b0525a16d281369f
khw970421 <khw970421@kakao.com>
2023-06-05 11:50:28 +0900

    makeDirectory 분리

---
+++ lolCrawl.js
@@ -0,0 +1,9 @@
+const { frontKeyword, keywordQueries } = require('./data/keywordQueries')
+const { crawling } = require('./utils/c')
+const { makeDirectory } = require('./utils/func')
+
+const crawlUrl = `https://www.leagueoflegends.com/ko-kr/champions/`
+const directory = process.argv[2] || 'img'
+
+makeDirectory(directory)
+crawling(directory, crawlUrl)
--- mkdir.js
+++
@@ -1,4 +0,0 @@
-const { makeDirectory } = require('./utils/func')
-
-const dir = process.argv[2] || 'img'
-makeDirectory(dir)
