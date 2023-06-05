fabdb93df5d26100657b4be620fc99ddf785bd92
khw970421 <khw970421@kakao.com>
2023-06-05 14:02:16 +0900

    makeDirectory 함수, 폴더 유무에 따라 분기 처리

--- utils/func.js
+++ utils/func.js
@@ -1,9 +1,18 @@
 const fs = require('fs')
 
-const makeDirectory = (directoryNm) => {
+const makeDirectory = (directoryNm, isDuplicatedDirRemove) => {
   const imgDirectory = fs.existsSync(`./${directoryNm}`)
-  if (imgDirectory) fs.rmSync(`./${directoryNm}`, { recursive: true })
-  fs.mkdirSync(`${directoryNm}`)
+  
+  // 폴더가 있으면서 이를 삭제하고 새로 생성
+  if (imgDirectory && isDuplicatedDirRemove) {
+    fs.rmSync(`./${directoryNm}`, { recursive: true })
+    fs.mkdirSync(`${directoryNm}`)
+  } 
+  // 폴더가 없을 때 새로 생성
+  else if (!imgDirectory) {
+    fs.mkdirSync(`${directoryNm}`)
+  }
+  // 폴더가 있으면서 이미 존재하는 폴더를 제거하고 싶지않으면 따로 수행 X
 }
 
 const getFileImageNm = (imgNm, idx) => {
