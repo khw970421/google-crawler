7ceb20fe214482dfc5e13e0c6b0212ad145d94f5
khw970421 <khw970421@kakao.com>
2023-06-19 14:15:53 +0900

    영어 챔피언 이름 받아오게 수정

--- lolCrawl.js
+++ lolCrawl.js
@@ -1,14 +1,16 @@
 const { keywordQueries } = require('./data/keywordQueries')
 const { crawling } = require('./utils/crawling')
 const { makeDirectory } = require('./utils/func')
+const champions = require('./data/champions.json')
+const championsArr = Object.keys(champions)
 
 const fs = {
   dir: 'img',
-  fileNm: keywordQueries || 'img',
+  fileNm: championsArr || 'img',
 }
 
 const search = {
-  url: `https://www.leagueoflegends.com/ko-kr/champions/`,
+  url: `https://www.leagueoflegends.com/en-us/champions/`,
   css: `.style__ImageContainer-n3ovyt-1 .cipsic`,
 }
 
